44adb854038ec00ede8356d5a71c82357047926b
Nikita <nsotnikov16@yandex.ru>
2021-12-10 12:44:25 +0500

    update

---
+++ Helix/js/script.js
@@ -0,0 +1,14 @@
+// Swiper Banners 
+var swiper = new Swiper(".swiper-banners", {
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + /* (index + 1) + */ "</span>";
+        },
+    },
+});
