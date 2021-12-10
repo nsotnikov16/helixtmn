c7340be3c29917518e99f3ba510cc184fad0e4cb
Nikita <nsotnikov16@yandex.ru>
2021-12-10 17:04:05 +0500

    update

--- Helix/js/script.js
+++ Helix/js/script.js
@@ -1,5 +1,6 @@
 // Swiper Banners 
 var swiper = new Swiper(".swiper-banners", {
+
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
@@ -12,3 +13,37 @@ var swiper = new Swiper(".swiper-banners", {
         },
     },
 });
+
+//tabs
+
+const tabs = document.querySelectorAll('.tabs')
+
+if (tabs.length > 0) {
+    tabs.forEach(element => {
+        const tabsControls = element.querySelector('.tabs__controls')
+        const tabsInputs = tabsControls.querySelectorAll('input')
+        const tabsContent = element.querySelector('.tabs__content')
+        const tabsTab = tabsContent.querySelectorAll('.tabs__tab')
+
+        tabsInputs.forEach((input, ind, arr) => {
+            if (input.checked) {
+                input.parentNode.classList.add('label--active')
+                tabsTab.forEach(tab => {
+                    if (tab.dataset.pointer === input.id) tab.classList.add('tabs__tab_active')
+                })
+            }
+
+            input.addEventListener('click', () => {
+                arr.forEach(el => el.parentNode.classList.remove('label--active'))
+                tabsTab.forEach(el => {
+                    el.classList.remove('tabs__tab_active')
+                    if (el.dataset.pointer === input.id) el.classList.add('tabs__tab_active')
+                })
+                input.parentNode.classList.add('label--active')
+            })
+        })
+
+
+    })
+
+}
